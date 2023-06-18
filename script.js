0c9e6fbed97b58a52c266e92a2580b532c436b23
VellyD <viddvd3@gmail.com>
2023-06-18 13:52:29 +0300

    removing fixed card and creating the DOM cards/dy

--- script.js
+++ script.js
@@ -1,14 +1,63 @@
+const resultsNav = document.getElementById("resultsNav");
+const favoritesNav = document.getElementById("favoritesNav");
+const imagesContainer = document.querySelector(".images-container");
+const saveConfirmed = document.querySelector(".save-confirmed");
+const loader = document.querySelector(".loader");
+
 const count = 10;
 const APIKey = "DEMO_KEY";
 const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${APIKey}&count=${count}`;
 
 let resultsArr = [];
 
+function updateDOM() {
+  resultsArr.forEach((result) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const link = document.createElement("a");
+    link.href = result.hdurl;
+    link.title = "View full image.";
+    link.target = "_blank";
+    const image = document.createElement("img");
+    image.src = result.url;
+    image.alt = "Nasa Picture of the day";
+    image.loading = "lazy";
+    image.classList.add("card-image-top");
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+    const cardTitle = document.createElement("h5");
+    cardTitle.classList.add("card-title");
+    cardTitle.textContent = result.title;
+    const saveText = document.createElement("p");
+    saveText.classList.add("clickable");
+    saveText.textContent = "Add to Favorites";
+    const cardText = document.createElement("p");
+    cardText.textContent = result.explanation;
+    const footer = document.createElement("small");
+    footer.classList.add("text-muted");
+    const date = document.createElement("strong");
+    date.textContent = result.date;
+    const copyRightResult =
+      result.copyright === undefined ? "" : result.copyright;
+    const copyRight = document.createElement("span");
+    copyRight.textContent = `${copyRightResult}`;
+
+    footer.append(date, copyRight);
+    cardBody.append(cardTitle, saveText, cardText, footer);
+    link.appendChild(image);
+    card.append(link, cardBody);
+
+    imagesContainer.appendChild(card);
+  });
+}
+
 async function getNasaPicturers() {
   try {
     const response = await fetch(apiUrl);
     resultsArr = await response.json();
-    console.log(resultsArr);
+
+    updateDOM();
   } catch (err) {}
 }
 
